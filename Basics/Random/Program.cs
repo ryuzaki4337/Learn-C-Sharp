0f1ae7b787014791c8d393bba719cd42dbea93e8
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 23:07:05 +0530

    Know how to generate random stuff

---
+++ Basics/Random/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for(var i=0;i<10;i++)
+            {
+                Console.WriteLine(random.Next());
+            }
+
+            for(var i=0;i<10;i++)
+            {
+                Console.WriteLine(random.Next(1,10));
+            }
+        }
+    }
+}
