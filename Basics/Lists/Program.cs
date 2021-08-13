0035257eeea086837fa6dc38a23c5998944b0fc3
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 23:06:33 +0530

    Know more about the dynamic sized arrays, 'Lists'

---
+++ Basics/Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>(){1,2,3,4};
+            numbers.Add(5);
+            numbers.AddRange(new int[4]{6,7,8,1});
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(numbers.IndexOf(1));
+
+            Console.WriteLine(numbers.LastIndexOf(1));
+
+            Console.WriteLine(numbers.Count);
+
+            numbers.Remove(1);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            numbers.Clear();
+
+            Console.WriteLine(numbers.Count);
+            
+        }
+    }
+}
