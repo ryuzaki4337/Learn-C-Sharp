6a8b52e8b87fd9ba9c009c3e64426c8f5987858b
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 21:15:08 +0530

    Learn about Arrays in this project

---
+++ Basics/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] S = new int[3];
+            S[0] = 1;
+            S[1] = 2;
+            S[2] = 3;
+
+            Console.WriteLine(S[0]);
+            Console.WriteLine(S[1]);
+            Console.WriteLine(S[2]);
+
+            string[] stringArray = new string[3] {"Jack","Jill","Mary"};
+
+            Console.WriteLine(stringArray[0]);
+            Console.WriteLine(stringArray[1]);
+            Console.WriteLine(stringArray[2]);
+        }
+    }
+}
