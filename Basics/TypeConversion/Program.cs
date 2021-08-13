6127e06212cac2ef0cad25a0b4971e202262ad8f
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 20:01:37 +0530

    Know how to convert type of a variable

---
+++ Basics/TypeConversion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Implicit Conversion
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(b); 
+            Console.WriteLine(i);
+
+            // Explicit Conversion
+            float f = 2.3f;
+            int v = (int)f;
+            Console.WriteLine(f);
+            Console.WriteLine(v);
+
+            // Non-compatible data types
+
+            string str = "1";
+            int m = Convert.ToInt32(str);
+            int n = int.Parse(str);
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+
+        }
+    }
+}
