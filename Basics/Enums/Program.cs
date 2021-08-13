7d3954ba7022f153f52c5faf9f6fdf6c82fdf3b5
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 21:16:28 +0530

    Know about enums

---
+++ Basics/Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+
+        public enum Months{
+            jan = 1,
+            feb,
+            mar,
+            apr,
+            may,
+            june,
+            july,
+            aug,
+            sept,
+            oct,
+            nov,
+            dec
+        }
+        static void Main(string[] args)
+        {
+            var octMonthNumber = Months.oct;
+            Console.WriteLine((int)octMonthNumber);
+        }
+    }
+}
