0e156af734acaf6a1ceb29fcb30d3d97057bfa96
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 20:01:02 +0530

    Know about Operators used in C#

---
+++ Basics/Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+            Console.WriteLine(a%b);
+            Console.WriteLine(a++);
+            Console.WriteLine(a);
+            Console.WriteLine(++b);
+            Console.WriteLine(b);
+            Console.WriteLine(a==b);
+            Console.WriteLine(b!=a);
+            Console.WriteLine(a>=b);
+            Console.WriteLine(a<=b);
+
+            var c = 3;
+            c += 5;
+            Console.WriteLine(c);
+            
+            
+            Console.WriteLine(c>b && c==a);
+            Console.WriteLine(c>b || c==a);
+            Console.WriteLine(!(c>b || c==a));
+        }
+    }
+}
