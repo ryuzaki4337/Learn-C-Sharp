be8cd4f9f34f61696bc29c4ecf22462b9a829eb4
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-13 20:02:10 +0530

    Know about different variables used in C#

---
+++ Basics/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte byteVar = 1;
+            Console.WriteLine(byteVar);
+
+            int intVar = 10;
+            Console.WriteLine(intVar);
+            
+            float floatVar = 1.2f;
+            Console.WriteLine(floatVar);
+            
+            char charVar = 'A';
+            Console.WriteLine(charVar);
+            
+            bool boolVar = true;
+            Console.WriteLine(boolVar);
+            
+            string stringVar = "Lalithaditya";
+            Console.WriteLine(stringVar);
+
+            var detectVar = "C_sharp is a cool language";
+            Console.WriteLine(detectVar);
+
+            Console.WriteLine("My name is {0}",stringVar);
+        }
+    }
+}
