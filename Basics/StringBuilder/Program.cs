91e9fa3dad5dd4853b7177749350ab8d210cf4a4
Kota Lalithaditya <lalithadityajmd10@gmail.com>
2021-08-14 06:35:54 +0530

    Learn how to manipulate String using StringBuilder

---
+++ Basics/StringBuilder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace stringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringB = new StringBuilder();
+            stringB.Append('-',10);
+            stringB.AppendLine();
+            stringB.Append("Lalithaditya");
+            stringB.AppendLine();
+            stringB.Append('-',10);
+
+
+            stringB.Replace('-','+');
+            Console.WriteLine(stringB);
+
+            stringB.Remove(0,10);
+            Console.WriteLine(stringB);
+
+            stringB.Insert(0,new string("+"),10);
+            Console.WriteLine(stringB);
+
+            stringB
+                .AppendLine()
+                .Append('*',10)
+                .Append("Yashwanth Kota")
+                .AppendLine()
+                .Append('*',10);
+
+            Console.WriteLine(stringB);
+        }
+    }
+}
